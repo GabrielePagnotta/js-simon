e2904ebb82fd1fdfd2f5a9593c40cc93344d721e
GabrielePagnottae <gabriele.pagnotta1998@gmail.com>
2022-11-03 14:34:48 +0100

    push1

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,41 @@
 // creo un array:
-function input(){
-    let array_1 = [];
+let array_1 = [];
+let show = document.getElementById('display')
 
-    for (let i = 0; i < 5 i++) {
-        let random = Math.floor(Math.random() * 5);
-        array_1.push(random[i])
-        
-    }
 
+// creo una funzione che mi dia un numero random:
+function casual_number(num){
+   let x = Math.floor(Math.random() * num)
+   return x
 }
 
-input();
+ //  creo un timer per la funzione display_info
 
-console.log(input())
+ setTimeout(display_info,5000);
+
+    // creo una funzione che stampa i numeri nell array
+
+    function display_info(){
+
+        for (let i = 0; i < 5; i++) {
+            casual_number()
+            array_1.push(casual_number(20));
+            show.innerHTML=`i numeri sono: ${array_1}`
+    
+        }
+    }
+
+    
+   
+
+    
+
+   
+
+
+
+
+
+
+    
+        
