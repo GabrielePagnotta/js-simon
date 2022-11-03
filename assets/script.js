52d3e7a0ef5f4e6c7fb3ef2f410ed26fe04a4f0b
GabrielePagnottae <gabriele.pagnotta1998@gmail.com>
2022-11-03 13:49:03 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// creo un array:
+function input(){
+    let array_1 = [];
+
+    for (let i = 0; i < 5 i++) {
+        let random = Math.floor(Math.random() * 5);
+        array_1.push(random[i])
+        
+    }
+
+}
+
+input();
+
+console.log(input())
